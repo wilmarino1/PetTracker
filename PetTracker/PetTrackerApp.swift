71ccecd4cfe9f7ff1666dc176279cda37b57d715
Wil Marino <wilmarino001@gmail.com>
2025-07-30 10:39:43 -0500

    Initial Commit

---
+++ PetTracker/PetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTrackerApp.swift
+//  PetTracker
+//
+//  Created by William Marino on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetTrackerTests/PetTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetTrackerTests.swift
+//  PetTrackerTests
+//
+//  Created by William Marino on 7/30/25.
+//
+
+import Testing
+@testable import PetTracker
+
+struct PetTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
